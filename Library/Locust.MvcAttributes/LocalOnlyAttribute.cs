3da928b3dd1928af2924c677054db28a4d94d654
Mansoor Omrani <mansoor.omrani@gmail.com>
2019-10-01 09:14:55 +0330

    Added LocalOnly action filter to restrict web requests to local

---
+++ Library/Locust.MvcAttributes/LocalOnlyAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Locust.MvcAttributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public class LocalOnlyAttribute : ActionFilterAttribute, IActionFilter
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.HttpContext.Request.IsLocal)
+            {
+                filterContext.Result = new HttpStatusCodeResult(HttpStatusCode.Unauthorized);
+            }
+        }
+    }
+}

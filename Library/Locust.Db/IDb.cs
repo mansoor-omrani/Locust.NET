53f5e6836aa51871928577e1124a74701d742742
Mansoor Omrani <mansoor.omrani@gmail.com>
2020-02-03 15:33:36 +0330

    Updated IDb (added namespace)

--- Library/Locust.Db/IDb.cs
+++ Library/Locust.Db/IDb.cs
@@ -1,8 +1,11 @@
 using System.Data.Common;
 
-public interface IDb
+namespace Locust.Db
+{
+    public interface IDb
     {
         DbConnection GetConnection();
         bool PersistConnection { get; set; }
         bool AutoNullEmptyStrings { get; set; }
     }
+}
